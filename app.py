00e7ce544d93236db5d75641baf1da5267feb65d
Thomas Mansencal <thomas.mansencal@gmail.com>
2018-08-12 19:28:40 +1200

    Raise app version.

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ __application_name__ = 'Colour - Analysis'
 
 __major_version__ = '0'
 __minor_version__ = '1'
-__change_version__ = '1'
+__change_version__ = '2'
 __version__ = '.'.join(
     (__major_version__,
      __minor_version__,
