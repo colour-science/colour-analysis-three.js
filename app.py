331b4be468c98ff01a0500fb0c016bab716d97bf
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-03-09 12:18:42 +1300

    Update copyright statements.

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ from colour_analysis import (
     visible_spectrum_visual)
 
 __author__ = 'Colour Developers'
-__copyright__ = 'Copyright (C) 2018 - Colour Developers'
+__copyright__ = 'Copyright (C) 2018-2019 - Colour Developers'
 __license__ = 'New BSD License - http://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-science@googlegroups.com'
