fd9e3b2d836b14d45b68863ad53f6e422429fd64
Thomas Mansencal <thomas.mansencal@gmail.com>
2020-01-22 20:32:58 +1300

    Update copyright statements.

--- colour_analysis.py
+++ colour_analysis.py
@@ -27,7 +27,7 @@ from colour.utilities import (CaseInsensitiveMapping, as_float_array,
 from colour.volume import XYZ_outer_surface
 
 __author__ = 'Colour Developers'
-__copyright__ = 'Copyright (C) 2018-2019 - Colour Developers'
+__copyright__ = 'Copyright (C) 2018-2020 - Colour Developers'
 __license__ = 'New BSD License - https://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-developers@colour-science.org'
