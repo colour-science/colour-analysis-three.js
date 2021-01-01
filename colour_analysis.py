6d7675710af2f376388cf4a9daa7fee43c1944e3
Thomas Mansencal <thomas.mansencal@gmail.com>
2021-01-01 14:25:17 +1300

    Update copyright statements.

--- colour_analysis.py
+++ colour_analysis.py
@@ -26,7 +26,7 @@ from colour.utilities import (as_float_array, first_item, normalise_maximum,
 from colour.volume import XYZ_outer_surface
 
 __author__ = 'Colour Developers'
-__copyright__ = 'Copyright (C) 2018-2020 - Colour Developers'
+__copyright__ = 'Copyright (C) 2018-2021 - Colour Developers'
 __license__ = 'New BSD License - https://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-developers@colour-science.org'
